1da44d86b5b120092d72529f3e3b4727263513ba
KIR <seiga@frl.mech.tohoku.ac.jp>
2018-04-18 13:49:52 +0900

    共通で使えるものを分離

---
+++ src/util.h
@@ -0,0 +1,47 @@
+#ifndef SYSMON_UTIL_H
+#define SYSMON_UTIL_H
+
+#include <ros/ros.h>
+#include <ros/console.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <numeric>
+
+namespace Sysmon{
+
+class Util{
+public:	
+	static bool readSingleLine(const std::string fname, std::string& outstr, const int32_t line=0) {
+		std::ifstream ifs;
+		std::string str;
+		int32_t cnt(line + 1);
+
+		ifs.open(fname.c_str());
+		if (ifs.fail()) {
+			ROS_INFO("Could not open a file");
+			return false;
+		}
+		while (cnt--) {
+			str.clear();
+			std::getline(ifs, str);
+			if (ifs.fail()) {
+				// End of file
+				//ROS_INFO("End of file");
+				return false;
+			}
+		}
+		ifs.close();
+
+		outstr = str;
+		return true;
+	}
+
+};
+
+}
+
+#endif /* SYSMON_UTIL_H */
+
