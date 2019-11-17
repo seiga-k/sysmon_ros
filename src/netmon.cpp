e22410d1f8321b3c5436759fedf2673b0a004d23
seiga-k <seiga@ieee.org>
2019-11-17 10:33:06 +0900

    fix netmon.cpp roslint error

--- src/netmon.cpp
+++ src/netmon.cpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright(c) 2019, seiga-k
+ * All rights reserved.
+ */
+
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <sysmon_ros/netif.h>
@@ -16,166 +21,197 @@
 #include <ifaddrs.h>
 #include <linux/if_link.h>
 
-#include "util.h"
+#include "./util.h"
 
-namespace Sysmon {
+namespace Sysmon
+{
 
-class Netmon {
+class Netmon
+{
 public:
+    Netmon() :
+    nh("~"),
+    hz(1.0),
+    do_loop(true)
+    {
+        int err_num;
+        if (getifaddrs(&ifaddr) == -1)
+        {
+            return;
+        }
 
-	Netmon() :
-	nh("~"),
-	hz(1.0),
-	do_loop(true)
-	{
-		int err_num;
-		if (getifaddrs(&ifaddr) == -1) {
-			return;
-		}
+        nh.getParam("hz", hz);
 
-		nh.getParam("hz", hz);
+        ifaddrs *ifa_;
+        for (ifa_ = ifaddr; ifa_ != NULL; ifa_ = ifa_->ifa_next)
+        {
+            if (ifa_->ifa_addr == NULL)
+            {
+                continue;
+            }
 
-		ifaddrs *ifa_;
-		for (ifa_ = ifaddr; ifa_ != NULL; ifa_ = ifa_->ifa_next) {
-			if (ifa_->ifa_addr == NULL) {
-				continue;
-			}
+            int family = ifa_->ifa_addr->sa_family;
+            std::string ifname(ifa_->ifa_name);
 
-			int family = ifa_->ifa_addr->sa_family;
-			std::string ifname(ifa_->ifa_name);
+            if (family == AF_INET)
+            {
+                char host[NI_MAXHOST];
+                err_num = getnameinfo(ifa_->ifa_addr,
+                    sizeof (sockaddr_in),
+                    host,
+                    NI_MAXHOST,
+                    NULL,
+                    0,
+                    NI_NUMERICHOST);
+                if (err_num != 0)
+                {
+                    ROS_WARN("getnameinfo() failed: %s", gai_strerror(err_num));
+                    return;
+                }
+                std::string ip(host);
+                ROS_INFO("Found network interface : %s [%s]", ifname.c_str(), ip.c_str());
+                auto it = ips.find(ifname);
+                if (it == ips.end())
+                {
+                    std::vector<std::string> ip_;
+                    ip_.push_back(ip);
+                    ips.insert(std::make_pair(ifname, ip_));
+                    pub_netifs.insert(
+                        std::make_pair(
+                            ifname,
+                            nh.advertise<sysmon_ros::netif>(
+                                std::regex_replace(ifname, std::regex(R"(\W)"), "_"), 1)));
+                    rtnl_link_stats st_tmp;
+                    if_stats.insert(std::make_pair(ifname, st_tmp));
+                }
+                else
+                {
+                    ips[ifname].push_back(ip);
+                }
+            }
+        }
+        for (ifa_ = ifaddr; ifa_ != NULL; ifa_ = ifa_->ifa_next)
+        {
+            if (ifa_->ifa_addr == NULL)
+            {
+                continue;
+            }
 
-			if (family == AF_INET) {
-				char host[NI_MAXHOST];
-				err_num = getnameinfo(ifa_->ifa_addr,
-					sizeof (sockaddr_in),
-					host,
-					NI_MAXHOST,
-					NULL,
-					0,
-					NI_NUMERICHOST);
-				if (err_num != 0) {
-					ROS_WARN("getnameinfo() failed: %s", gai_strerror(err_num));
-					return;
-				}
-				std::string ip(host);
-				ROS_INFO("Found network interface : %s [%s]", ifname.c_str(), ip.c_str());
-				auto it = ips.find(ifname);
-				if (it == ips.end()) {
-					std::vector<std::string> ip_;
-					ip_.push_back(ip);
-					ips.insert(std::make_pair(ifname, ip_));
-					pub_netifs.insert(std::make_pair(ifname, nh.advertise<sysmon_ros::netif>(std::regex_replace(ifname, std::regex(R"(\W)"), "_"), 1)));
-					rtnl_link_stats st_tmp;
-					if_stats.insert(std::make_pair(ifname, st_tmp));
-				} else {
-					ips[ifname].push_back(ip);
-				}
-			}
-		}
-		for (ifa_ = ifaddr; ifa_ != NULL; ifa_ = ifa_->ifa_next) {
-			if (ifa_->ifa_addr == NULL) {
-				continue;
-			}
+            int family = ifa_->ifa_addr->sa_family;
+            std::string ifname(ifa_->ifa_name);
 
-			int family = ifa_->ifa_addr->sa_family;
-			std::string ifname(ifa_->ifa_name);
+            if (if_stats.count(ifname) == 0)
+            {
+                continue;
+            }
 
-			if (if_stats.count(ifname) == 0){
-				continue;
-			}
+            if (family == AF_PACKET && ifa_->ifa_data != NULL)
+            {
+                rtnl_link_stats *stats = static_cast<rtnl_link_stats *>(ifa_->ifa_data);
+                if_stats[ifname] = *stats;
+            }
+        }
+        ROS_INFO("Start netmon node");
+    }
 
-			if (family == AF_PACKET && ifa_->ifa_data != NULL) {
-				rtnl_link_stats *stats = (rtnl_link_stats *) ifa_->ifa_data;
-				if_stats[ifname] = *stats;
-			}
-		}
-		ROS_INFO("Start netmon node");
-	}
+    ~Netmon()
+    {
+        freeifaddrs(ifaddr);
+    }
 
-	~Netmon()
-	{
-		freeifaddrs(ifaddr);
-	}
+    void run()
+    {
+        ros::Time last_time = ros::Time::now();
+        ros::Rate rate(hz);
+        while (ros::ok() && do_loop)
+        {
+            ros::spinOnce();
 
-	void run()
-	{
-		ros::Time last_time = ros::Time::now();
-		ros::Rate rate(hz);
-		while (ros::ok() && do_loop) {
-			ros::spinOnce();
+            ros::Time cur_time = ros::Time::now();
+            ros::Duration diff = cur_time - last_time;
+            double diff_sec = diff.toSec();
 
-			ros::Time cur_time = ros::Time::now();
-			ros::Duration diff = cur_time - last_time;
-			double diff_sec = diff.toSec();
+            if (getifaddrs(&ifaddr) == -1)
+            {
+                return;
+            }
 
-			if (getifaddrs(&ifaddr) == -1) {
-				return;
-			}
+            ifaddrs *ifa_;
+            for (ifa_ = ifaddr; ifa_ != NULL; ifa_ = ifa_->ifa_next)
+            {
+                if (ifa_->ifa_addr == NULL)
+                {
+                    continue;
+                }
 
-			ifaddrs *ifa_;
-			for (ifa_ = ifaddr; ifa_ != NULL; ifa_ = ifa_->ifa_next) {
-				if (ifa_->ifa_addr == NULL) {
-					continue;
-				}
+                int family = ifa_->ifa_addr->sa_family;
+                std::string ifname(ifa_->ifa_name);
 
-				int family = ifa_->ifa_addr->sa_family;
-				std::string ifname(ifa_->ifa_name);
+                if (if_stats.count(ifname) == 0)
+                {
+                    continue;
+                }
 
-				if (if_stats.count(ifname) == 0){
-					continue;
-				}
+                if (family == AF_PACKET && ifa_->ifa_data != NULL)
+                {
+                    rtnl_link_stats *stats = static_cast<rtnl_link_stats *>(ifa_->ifa_data);
+                    int32_t tx_bps(0.0);
+                    int32_t rx_bps(0.0);
+                    double tx_err_rate(0.0);
+                    double rx_err_rate(0.0);
+                    sysmon_ros::netif msg;
 
-				if (family == AF_PACKET && ifa_->ifa_data != NULL) {
-					rtnl_link_stats *stats = (rtnl_link_stats *) ifa_->ifa_data;
-					int32_t tx_bps(0.0);
-					int32_t rx_bps(0.0);
-					double tx_err_rate(0.0);
-					double rx_err_rate(0.0);
-					sysmon_ros::netif msg;
+                    tx_bps = static_cast<double>(stats->tx_bytes - if_stats[ifname].tx_bytes)
+                                * 8.0 / diff_sec;
+                    rx_bps = static_cast<double>(stats->rx_bytes - if_stats[ifname].rx_bytes)
+                                * 8.0 / diff_sec;
+                    if (stats->tx_packets > if_stats[ifname].tx_packets)
+                    {
+                        tx_err_rate = static_cast<double>(stats->tx_errors - if_stats[ifname].tx_errors)
+                                        / static_cast<double>(stats->tx_packets - if_stats[ifname].tx_packets)
+                                        * 100.0;
+                    }
+                    if (stats->rx_packets > if_stats[ifname].rx_packets)
+                    {
+                        rx_err_rate = static_cast<double>(stats->rx_errors - if_stats[ifname].rx_errors)
+                                        / static_cast<double>(stats->rx_packets - if_stats[ifname].rx_packets)
+                                        * 100.0;
+                    }
+                    msg.if_name = ifname;
+                    msg.ip = ips[ifname];
+                    msg.rx_bps = rx_bps;
+                    msg.rx_error_rate = rx_err_rate;
+                    msg.tx_bps = tx_bps;
+                    msg.tx_error_rate = tx_err_rate;
+                    pub_netifs[ifname].publish(msg);
+                    ROS_DEBUG("%1.3lf %s\t\trate : %10d, %10d, %3.2f, %3.2f",
+                                diff_sec, ifname.c_str(), tx_bps, rx_bps, tx_err_rate, rx_err_rate);
 
-					tx_bps = (double) (stats->tx_bytes - if_stats[ifname].tx_bytes) * 8.0 / diff_sec;
-					rx_bps = (double) (stats->rx_bytes - if_stats[ifname].rx_bytes) * 8.0 / diff_sec;
-					if (stats->tx_packets > if_stats[ifname].tx_packets) {
-						tx_err_rate = (double) (stats->tx_errors - if_stats[ifname].tx_errors) / (double) (stats->tx_packets - if_stats[ifname].tx_packets) * 100.0;
-					}
-					if (stats->rx_packets > if_stats[ifname].rx_packets) {
-						rx_err_rate = (double) (stats->rx_errors - if_stats[ifname].rx_errors) / (double) (stats->rx_packets - if_stats[ifname].rx_packets) * 100.0;
-					}
-					msg.if_name = ifname;
-					msg.ip = ips[ifname];
-					msg.rx_bps = rx_bps;
-					msg.rx_error_rate = rx_err_rate;
-					msg.tx_bps = tx_bps;
-					msg.tx_error_rate = tx_err_rate;
-					pub_netifs[ifname].publish(msg);
-					ROS_DEBUG("%1.3lf %s\t\trate : %10d, %10d, %3.2f, %3.2f", diff_sec, ifname.c_str(), tx_bps, rx_bps, tx_err_rate, rx_err_rate);
+                    if_stats[ifname] = *stats;
+                }
+            }
+            last_time = cur_time;
 
-					if_stats[ifname] = *stats;
-				}
-			}
-			last_time = cur_time;
-
-			rate.sleep();
-		}
-
-	}
+            rate.sleep();
+        }
+    }
 
 private:
-	ros::NodeHandle nh;
-	std::map<std::string, ros::Publisher> pub_netifs;
-	std::map<std::string, rtnl_link_stats> if_stats;
-	std::map<std::string, std::vector<std::string>> ips;
-	ifaddrs *ifaddr;
-	double hz;
-	bool do_loop;
+    ros::NodeHandle nh;
+    std::map<std::string, ros::Publisher> pub_netifs;
+    std::map<std::string, rtnl_link_stats> if_stats;
+    std::map<std::string, std::vector<std::string>> ips;
+    ifaddrs *ifaddr;
+    double hz;
+    bool do_loop;
 };
-}
+}   // namespace Sysmon
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "netmon");
-	Sysmon::Netmon netmon;
-	netmon.run();
-	ros::spin();
-	return 0;
+    ros::init(argc, argv, "netmon");
+    Sysmon::Netmon netmon;
+    netmon.run();
+    ros::spin();
+    return 0;
 }
